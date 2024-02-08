fa5d5a2ddd7e6ea298132cffb38223ff90a1cc16
itsbluejelly <ronniedenzel0@gmail.com>
2024-02-08 04:46:21 +0300

    fixed location

--- src/hooks/RootVerifier.tsx
+++ src/hooks/RootVerifier.tsx
@@ -8,18 +8,18 @@ import {UserType} from "@/types/Types"
 
 // A HOOK THAT VALIDATES THE ACCESSED ROUTE IN TERMS OF AUTHENTICATION
 export default function RootVerifier(user: UserType | null, path: "/home" | "/auth"): void{
-    // CHECKING IF THIS CODE IS RUNNING ON THE CLIENT SIDE
-    if (typeof window === 'undefined') {
-        // Don't execute on the server side
-        return;
-    }
-    // INSTANTIATING THE ROUTER
-    const router = useRouter()
+  // CHECKING IF THIS CODE IS RUNNING ON THE CLIENT SIDE
+  if (typeof window === "undefined") {
+    // Don't execute on the server side
+    return;
+  }
+  // INSTANTIATING THE ROUTER
+  const router = useRouter();
 
-    // PERFORMING NECESSARY REROUTING ON CLIENT SIDE
-    if(!user && path === "/home"){
-        router.push('/auth/login')
-    }else if (user && path === "/auth") {
-      router.push("/home");
-    }
+  // PERFORMING NECESSARY REROUTING ON CLIENT SIDE
+  if (!user && path === "/home") {
+    router.push("/auth/login");
+  } else if (user && path === "/auth") {
+    router.push("/home");
+  }
 }
