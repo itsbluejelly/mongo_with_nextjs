66d360d22537b9856c8526a95636f857f2f11f0f
itsbluejelly <ronniedenzel0@gmail.com>
2024-02-08 04:53:13 +0300

    added fix to hook issue

--- src/hooks/RootVerifier.tsx
+++ src/hooks/RootVerifier.tsx
@@ -2,24 +2,22 @@
 
 // IMPORTING NECESSARY FILES
     // IMPORTING MODULES
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
     // IMPORTING TYPES
 import {UserType} from "@/types/Types"
 
 // A HOOK THAT VALIDATES THE ACCESSED ROUTE IN TERMS OF AUTHENTICATION
 export default function RootVerifier(user: UserType | null, path: "/home" | "/auth"): void{
-  // CHECKING IF THIS CODE IS RUNNING ON THE CLIENT SIDE
-  if (typeof window === "undefined") {
-    // Don't execute on the server side
-    return;
-  }
-  // INSTANTIATING THE ROUTER
-  const router = useRouter();
+    // INSTANTIATING THE ROUTER
+    const router = useRouter();
 
-  // PERFORMING NECESSARY REROUTING ON CLIENT SIDE
-  if (!user && path === "/home") {
-    router.push("/auth/login");
-  } else if (user && path === "/auth") {
-    router.push("/home");
-  }
+    // CHECKING IF THIS CODE IS RUNNING ON THE CLIENT SIDE
+    if (typeof window === "undefined") {
+            // PERFORMING NECESSARY REROUTING ON CLIENT SIDE
+        if (!user && path === "/home") {
+            router.push("/auth/login");
+        } else if (user && path === "/auth") {
+            router.push("/home");
+        }
+    }
 }
