fdd2241ead4d24943df3ee6cede8386a3394e802
itsbluejelly <ronniedenzel0@gmail.com>
2024-02-08 18:19:17 +0300

    fixed api endpoint

--- src/app/home/addTopic/page.tsx
+++ src/app/home/addTopic/page.tsx
@@ -93,7 +93,7 @@ export default function AddTopicPage() {
 
         try {
           const response: Response = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/notes/note`,
+            `${process.env.NEXT_PUBLIC_API_URL}/notes`,
             {
               method: "POST",
               headers: { "Content-Type": "application/json" },
