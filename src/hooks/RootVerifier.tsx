10277514314bfaf4dc7a426f40bb3c5a96c40b39
itsbluejelly <ronniedenzel0@gmail.com>
2024-02-08 04:43:52 +0300

    added server check

--- src/hooks/RootVerifier.tsx
+++ src/hooks/RootVerifier.tsx
@@ -8,6 +8,11 @@ import {UserType} from "@/types/Types"
 
 // A HOOK THAT VALIDATES THE ACCESSED ROUTE IN TERMS OF AUTHENTICATION
 export default function RootVerifier(user: UserType | null, path: "/home" | "/auth"): void{
+    // CHECKING IF THIS CODE IS RUNNING ON THE CLIENT SIDE
+    if (typeof window === 'undefined') {
+        // Don't execute on the server side
+        return;
+    }
     // INSTANTIATING THE ROUTER
     const router = useRouter()
 
